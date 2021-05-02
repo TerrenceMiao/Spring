179f2f4646fcdc8f715a10bc9413efdb1e3d14d0
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2021-05-02 20:54:15 +1000

    Add groovy based unit test

---
+++ httpclient/src/test/groovy/org.paradise.net.httpclient.health/OrchestrationApiHealthIndicatorSpec.groovy
@@ -0,0 +1,15 @@
+package org.paradise.net.httpclient.health
+
+import spock.lang.Specification
+
+class OrchestrationApiHealthIndicatorSpec extends Specification {
+
+    void setup() {
+    }
+
+    void cleanup() {
+    }
+
+    def "DoHealthCheck"() {
+    }
+}

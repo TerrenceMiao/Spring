73b6bc337d935143dc45372639018735adca89d9
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2021-05-03 00:01:28 +1000

    Add Orchestration API health check unit test

--- httpclient/src/test/groovy/org.paradise.net.httpclient.health/OrchestrationApiHealthIndicatorSpec.groovy
+++ httpclient/src/test/groovy/org.paradise.net.httpclient.health/OrchestrationApiHealthIndicatorSpec.groovy
@@ -1,15 +1,90 @@
 package org.paradise.net.httpclient.health
 
+import com.google.common.collect.ImmutableMap
+import org.apache.commons.lang3.RandomStringUtils
+import org.paradise.net.httpclient.api.ApiHealth
+import org.paradise.net.httpclient.ochestrationApi.OrchestrationApi
+import org.springframework.boot.actuate.health.Status
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import retrofit2.Call
+import retrofit2.OkHttpCall
 import spock.lang.Specification
+import spock.lang.Subject
+
+import static java.lang.String.format
+import static retrofit2.Response.error
+import static retrofit2.Response.success
 
 class OrchestrationApiHealthIndicatorSpec extends Specification {
 
-    void setup() {
+    def username = RandomStringUtils.random(8)
+    def password = RandomStringUtils.random(8)
+
+    def headers = ImmutableMap.<String, String> builder()
+            .put(HttpHeaders.AUTHORIZATION, format("Basic %s", Base64.getEncoder().encodeToString(format("%s:%s", username, password).getBytes())))
+            .build()
+
+    def mockOrchestrationApi = Mock(OrchestrationApi)
+
+    @Subject
+    def orchestrationApiHealthIndicator = new OrchestrationApiHealthIndicator(username, password, mockOrchestrationApi)
+
+    def "should return health check up"() {
+
+        given:
+        mockOrchestrationApi.getHealth(headers) >> successfulResponse()
+
+        when:
+        def healthIndicator = orchestrationApiHealthIndicator.health()
+
+        then:
+        healthIndicator.status == Status.UP
     }
 
-    void cleanup() {
+    def "should return health check down"() {
+
+        given:
+        mockOrchestrationApi.getHealth(headers) >> unsuccessfulResponse()
+
+        when:
+        def healthIndicator = orchestrationApiHealthIndicator.health()
+
+        then:
+        healthIndicator.status == Status.DOWN
     }
 
-    def "DoHealthCheck"() {
+    def "should return health check down on error"() {
+
+        given:
+        mockOrchestrationApi.getHealth(headers) >> errorResponse()
+
+        when:
+        def healthIndicator = orchestrationApiHealthIndicator.health()
+
+        then:
+        healthIndicator.status == Status.DOWN
+    }
+
+    private Call<ApiHealth> successfulResponse() {
+        Mock(Call) {
+            execute() >> success(createResponseBody(Status.UP))
+        }
+    }
+
+    private Call<ApiHealth> unsuccessfulResponse() {
+        Mock(Call) {
+            execute() >> error(HttpStatus.SERVICE_UNAVAILABLE.value(), new OkHttpCall.NoContentResponseBody(null, 0))
+        }
+    }
+
+    private Call<ApiHealth> errorResponse() {
+        Mock(Call) {
+            execute() >> error(HttpStatus.BAD_REQUEST.value(), new OkHttpCall.NoContentResponseBody(null, 0))
+        }
+    }
+
+    private static ApiHealth createResponseBody(Status status) {
+        return new ApiHealth().setStatus(status.toString())
     }
 }
